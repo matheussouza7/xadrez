fb846f9cb31893ee3e5e8e864ca7706950c86008
Matheus Souza <msouza7_@outlook.com>
2020-07-25 00:19:06 -0300

    Configurada a classe Posicao

--- xadrez-console/Tabuleiro/Posicao.cs
+++ xadrez-console/Tabuleiro/Posicao.cs
@@ -2,9 +2,21 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace xadrez_console.Tabuleiro
+namespace Tabuleiro
 {
     class Posicao
     {
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
+
+        public Posicao(int linha, int coluna) {
+            Linha = linha;
+            Coluna = coluna;
+        }
+
+        public override string ToString()
+        {
+            return Linha + "," + Coluna;
+        }
     }
 }
